2bd2b079870375709cbb0cd52e3c837d4945ecb7
Edgard Alas <edgardalasu@gmail.com>
2024-12-13 23:44:23 -0600

    feat: restructure pages, add new components, and update styles

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const DashboardHomePage = () => {
+	return (
+		<div>
+			<Button>Hello, World!</Button>
+		</div>
+	);
+};
+
+export default DashboardHomePage;
