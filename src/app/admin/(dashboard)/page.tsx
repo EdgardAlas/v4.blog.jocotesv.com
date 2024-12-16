8086221bb9d3ddef1472a58bf17b214c7bb4b285
Edgard Alas <edgardalasu@gmail.com>
2024-12-16 14:10:23 -0600

    feat: update dashboard page content and adjust admin header for mobile view

--- src/app/admin/(dashboard)/page.tsx
+++ src/app/admin/(dashboard)/page.tsx
@@ -1,44 +1,13 @@
-'use client';
-
 import { AdminHeader } from '@/components/layout/admin-header';
-import { Button } from '@/components/ui/button';
 import { SimpleCard } from '@/components/ui/card';
-import { useConfirm } from '@/components/ui/confirm-dialog';
-import { MinimalTiptapEditor } from '@/components/ui/minimal-tiptap';
-import { Content } from '@tiptap/react';
-import { useState } from 'react';
 
 const DashboardHomePage = () => {
-	const confirm = useConfirm();
-	const [value, setValue] = useState<Content>('');
 	return (
 		<>
 			<AdminHeader title='Dashboard' />
 			<SimpleCard>
-				<div className='flex items-center justify-between'>
-					<h2 className='text-xl font-semibold'>Welcome to the dashboard</h2>
-					<Button
-						onClick={async () => {
-							await confirm({
-								title: 'Confirm Action',
-								description: 'Are you sure you want to proceed?',
-							});
-						}}
-					>
-						Click me
-					</Button>
-				</div>
-
-				<MinimalTiptapEditor
-					value={value}
-					onChange={setValue}
-					className='w-full'
-					editorContentClassName='p-5'
-					output='html'
-					placeholder='Type your description here...'
-					autofocus={true}
-					editorClassName='focus:outline-none'
-				/>
+				<h2 className='text-xl font-semibold'>Welcome to the dashboard</h2>
+				<p>This site is under construction. Please check back later.</p>
 			</SimpleCard>
 		</>
 	);
--- src/components/layout/admin-header.tsx
+++ src/components/layout/admin-header.tsx
@@ -1,5 +1,4 @@
 import { CustomToggleSidebar } from '@/components/layout/custom-toggle-sidebar';
-import { useSidebar } from '@/components/ui/sidebar';
 import { DateTime } from 'luxon';
 
 interface PageHeaderProps {
@@ -7,11 +6,12 @@ interface PageHeaderProps {
 }
 
 export const AdminHeader = ({ title }: PageHeaderProps) => {
-	const { isMobile } = useSidebar();
 	return (
 		<header className='flex h-16 items-center justify-between gap-4 bg-background py-4'>
 			<div className='flex items-center gap-2'>
-				{isMobile && <CustomToggleSidebar />}
+				<div className='md:hidden'>
+					<CustomToggleSidebar />
+				</div>
 				<h1 className='text-2xl font-bold'>{title}</h1>
 			</div>
 			<time dateTime={DateTime.now().toISO()} className='text-sm font-bold'>
